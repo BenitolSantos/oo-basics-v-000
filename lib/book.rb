6f08f376b59bb51404fb9d22cc9067b0daa8a1b2
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-20 05:47:07 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,8 +4,8 @@ class Book
     @title = title
   end
 
-  attr_accessor :author,:page_count,:genre
-  attr_reader :title
+  attr_accessor :author,:page_count,:genre #getter/setter for mulitple attributes
+  attr_reader :title #getter getting title
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
