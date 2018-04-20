8a17911a83815d60f2fc11255733f145ff354764
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-20 05:12:55 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,13 +4,11 @@ class Book
     @title = title
   end
 
-  attr_accessor :title
-  attr_accessor :author
-  attr_accessor :page_count
+  attr_accessor :title,:author,:page_count
   attr_accessor :genre
 
   def turn_page
-    puts "Flipping the page...wow, you read fast!"    
+    puts "Flipping the page...wow, you read fast!"
   end
 
 end
